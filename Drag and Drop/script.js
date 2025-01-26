30349a3d4a4818e3d6dad64603ad051996843880
Lê Khánh Trình <vulekhanhtrinh2003@gmail.com>
2025-01-26 15:18:50 +0700

    drag & drop update

--- Drag and Drop/script.js
+++ Drag and Drop/script.js
@@ -1,30 +1,10 @@
-const mal = document.querySelector(".mal");
-const ziggs = document.querySelector(".ziggs");
-const brand = document.querySelector(".brand");
+const excludeClasses = ["dragging", "invisible", "hovered"];
 
-const ahri = document.querySelector(".ahri");
-const corki = document.querySelector(".corki");
-const ezreal = document.querySelector(".ezreal");
-
+const pics = document.querySelectorAll(".pic");
 const boxes = document.querySelectorAll(".box");
 
-mal.addEventListener("dragstart", dragStart);
-mal.addEventListener("dragend", dragEnd);
-
-ziggs.addEventListener("dragstart", dragStart);
-ziggs.addEventListener("dragend", dragEnd);
-
-brand.addEventListener("dragstart", dragStart);
-brand.addEventListener("dragend", dragEnd);
-
-ahri.addEventListener("dragstart", dragStart);
-ahri.addEventListener("dragend", dragEnd);
-
-corki.addEventListener("dragstart", dragStart);
-corki.addEventListener("dragend", dragEnd);
-
-ezreal.addEventListener("dragstart", dragStart);
-ezreal.addEventListener("dragend", dragEnd);
+let currentListClass = null;
+let currentListClassHover = null;
 
 for (const box of boxes) {
   box.addEventListener("dragover", dragOver);
@@ -32,29 +12,75 @@ for (const box of boxes) {
   box.addEventListener("dragleave", dragLeave);
   box.addEventListener("drop", dragDrop);
 }
+for (const pic of pics) {
+  pic.addEventListener("dragstart", dragStart);
+  pic.addEventListener("dragend", dragEnd);
+}
 
+// Function to handle the start of dragging
 function dragStart() {
-  setTimeout(() => (this.className = "invisible"), 0);
+  this.className += " dragging";
+  setTimeout(() => (this.className += " invisible"), 0);
+  currentListClass = Array.from(this.classList).filter(
+    (className) => !excludeClasses.includes(className)
+  );
 }
 
+// Function to handle the end of dragging
 function dragEnd() {
-  this.className = "mal";
+  currentListClass.forEach((className) => this.classList.add(className));
 }
 
+// Function to handle the drag over event
 function dragOver(e) {
   e.preventDefault();
 }
 
+// Function to handle the drag enter event
 function dragEnter(e) {
   e.preventDefault();
-  this.className += " hovered";
+
+  const childElement = this.querySelector(".pic");
+  currentListClassHover = Array.from(childElement.classList).filter(
+    (className) => !excludeClasses.includes(className)
+  );
+  childElement.classList.add("hovered");
 }
 
-function dragLeave() {
-  this.className = "box";
+// Function to handle the drag leave event
+function dragLeave(e) {
+  if (e.relatedTarget && this.contains(e.relatedTarget)) return;
+
+  currentListClassHover = null;
+  this.querySelector(".pic").classList.remove("hovered");
 }
 
+// Function to handle the drop event
 function dragDrop() {
-  this.className = "box";
-  this.append(mal);
+  const draggedElement = document.querySelector(".dragging");
+  const targetElement = this.querySelector(".pic");
+
+  if (draggedElement && targetElement) {
+    const parent1 = draggedElement.parentNode;
+    const parent2 = targetElement.parentNode;
+
+    swapChildren(parent1, draggedElement, parent2, targetElement);
+  }
+
+  this.classList.remove("hovered");
+}
+
+// Function to swap children between two parent elements
+function swapChildren(parent1, child1, parent2, child2) {
+  // refine class names
+  child1.className = "";
+  child2.className = "";
+
+  currentListClassHover.forEach((className) => child2.classList.add(className));
+  currentListClass.forEach((className) => child1.classList.add(className));
+  // Move child1 to parent2
+  parent2.appendChild(child1);
+
+  // Move child2 to parent1
+  parent1.appendChild(child2);
 }
